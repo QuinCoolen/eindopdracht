7bd18deb0cdf520af5e67dbfd1165dc825f85587
Quin Coolen <quin.coolen@gmail.com>
2021-11-05 13:02:18 +0100

    show indiviual

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -46,7 +46,7 @@ class EventController extends Controller
      */
     public function show($id)
     {
-        //
+        return view("events.show", ['event' => Event::find($id)]);
     }
 
     /**
---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>ID: {{$event->id}}</li>
+        <li>Name: {{$event->name}}</li>
+        <li>Date: {{$event->date}}</li>
+        <li>Description: {{$event->description}}</li>
+    </ul>
+</body>
+</html>
